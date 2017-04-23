6a30cfb8443003f115fccd8f489f91c2f57dce8d
jgmontoya <jgmontoya@uc.cl>
2017-04-23 16:01:30 -0300

    Users CRUD Initial commit

--- series4u/config/routes.rb
+++ series4u/config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   as :user do
     get 'login' => 'users/sessions#new', as: :new_user_session
     post 'login' => 'users/sessions#create', as: :user_session
-    delete 'logout' => 'users/sessions#destroy', as: :destroy_user_session
+    get 'logout' => 'users/sessions#destroy', as: :destroy_user_session
     get 'register' => 'users/registrations#new'
   end
 
