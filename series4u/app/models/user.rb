11ff2579af9d40e38eb50c8ef41d355ae5581a51
jgmontoya <jgmontoya@uc.cl>
2017-04-29 18:39:25 -0300

    Minor fix to model

--- series4u/app/models/user.rb
+++ series4u/app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
 
   has_many :chapters
   has_many :series
-  has_many :views
+  has_many :views, dependent: :destroy
   has_many :chapter_ratings, through: :views
   has_many :chapter_reviews, through: :views
   has_many :serie_reviews
