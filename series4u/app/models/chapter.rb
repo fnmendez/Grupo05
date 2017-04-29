7327f08d82ef67ad10b38ff1fabcb247d5abd392
jgmontoya <jgmontoya@uc.cl>
2017-04-29 18:42:50 -0300

    Minor fix to models

--- series4u/app/models/chapter.rb
+++ series4u/app/models/chapter.rb
@@ -1,11 +1,11 @@
 class Chapter < ApplicationRecord
   belongs_to :user
   belongs_to :season
-  has_many :chapter_acts
-  has_many :chapter_directeds
+  has_many :chapter_acts, dependent: :destroy
+  has_many :chapter_directeds, dependent: :destroy
   has_many :actors, through: :chapter_acts
   has_many :directors, through: :chapter_directeds
-  has_many :views
+  has_many :views, dependent: :destroy
   validates :title, presence: true
   validates :user, presence: true
   validates :season, presence: true
--- series4u/app/models/director.rb
+++ series4u/app/models/director.rb
@@ -1,4 +1,6 @@
 class Director < ApplicationRecord
+  has_many :chapter_directeds, dependent: :destroy
+  has_many :serie_directeds, dependent: :destroy
   has_many :chpaters, through: :chapter_directeds
   has_many :series, through: :serie_directeds
 end
