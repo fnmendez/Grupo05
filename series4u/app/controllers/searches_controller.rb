ffb21fe5d64422da59d62ef004d1ad4e7978e2da
Andres Mardones <agmardones@uc.cl>
2017-06-25 13:24:46 -0400

    Searches controller update for sorting

--- series4u/app/controllers/searches_controller.rb
+++ series4u/app/controllers/searches_controller.rb
@@ -3,11 +3,34 @@ class SearchesController < ApplicationController
 	end
 	def results
 		if params[:search]
+			@title = params[:search]
+			@genre = ""
 			@series = Serie.search_by_title(params[:search], current_user)
 			@chapters = Chapter.search(params[:search], current_user)
-		elsif params[:genre]
+		end
+		if params[:genre]
+			@title = params[:title]
+			@genre = params[:genre]
 			@series = Serie.search(params[:title], params[:genre], current_user)
 			@chapters = Chapter.search(params[:title], current_user)
+		end
+		if params[:sort]
+			@series = Serie.search(params[:title], params[:genre], current_user)
+			@chapters = Chapter.search(params[:title], current_user)
+			@to_sort = params[:sort].split("-")[0]
+			@criterio = params[:sort].split("-")[1]
+			if @criterio == "user"
+				@criterio = "user.username"
+			end
+			if  @criterio == "from"
+				@criterio = "season.serie.title"
+			end
+			@criterio = @criterio.to_sym
+			if @to_sort == "series"
+				@series = @series.sort_by { |s| s[@criterio] }
+			elsif @to_sort == "chapters"
+				@chapters = @chapters.sort_by { |c| c[@criterio] }
+			end
 		end
 	end
 end
