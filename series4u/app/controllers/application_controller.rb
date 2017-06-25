ce4522631141b6e8e1e0af00dd78d0123ad38997
jgmontoya <jgmontoya@uc.cl>
2017-06-25 19:40:22 -0400

    Last seen chapter button added to navbar

--- series4u/app/controllers/application_controller.rb
+++ series4u/app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :last_chapter
+
+  def last_chapter
+    unless user_signed_in?
+      return root_path
+    end
+    last_view = View.where(user:current_user).last
+    @chapter = last_view.nil?? root_path : last_view.chapter
+  end
 end
